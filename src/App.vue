895952582bf789a71109bab037d81985fe5f09f1
robert <robert@unknown020fb576a7ca.attlocal.net>
2023-03-17 16:52:54 -0700

    added logic to render memo data

--- src/App.vue
+++ src/App.vue
@@ -2,7 +2,21 @@
 import { ref } from "vue";
 
 const showModel = ref(false);
+const newMemo = ref("");
+const newTitle = ref("");
+const memos = ref([]);
 
+const addMemo = () => {
+  memos.value.push({
+    id: Math.floor(Math.random() * 100),
+    input: newTitle.value,
+    text: newMemo.value,
+    date: new Date(),
+ });
+ showModel.value = false;
+ newMemo.value = ""
+ newTitle.value = ""
+}
 </script>
 
 <template>
@@ -11,34 +25,31 @@ const showModel = ref(false);
     <div v-if="showModel" class="add_modal_screen">
       <div class="modal">
         <p class="leadText">Add a memo:</p>
-        <input placeholder="Title" />
+        <input v-model="newTitle" placeholder="Title" />
         <textarea
           name="memos"
           id="memos"
           cols="30"
           rows="10"
           placeholder="What is your memo?"
+          v-model="newMemo"
         ></textarea>
-        <button class="modal_button">Add Memo</button>
+        <button @click="addMemo()" class="modal_button">Add Memo</button>
         <button @click="showModel = false" class="modal_close">close</button>
       </div>
     </div>
     <h4 class="leadText">Add a new memo</h4>
     <button @click="showModel = true">+</button>
   </header>
-
   <main>
     <p class="your_memos">Memos:</p>
     <div class="card-container">
-      <div class="card">
-        <p class="date">04/34/20002</p>
-        <p class="memoTitle">Title</p>
-        <p class="main-text">
-          Lorem ipsum dolor sit amet consectetur, adipisicing elit. Ipsum culpa
-          ab id dolorum inventore! Blanditiis culpa necessitatibus earum iure
-          similique!
-        </p>
-        <button>Delete</button>
+      <div v-for="memo in memos" class="card">
+        <p class="date">{{ memo.date.toLocaleDateString("en-US") }}</p>
+        <p class="memo_title">Title: {{ memo.input }}</p>
+        <p class="main_text">Memo:
+          {{ memo.text }}</p>
+        <button class="del_button">Delete</button>
       </div>
     </div>
   </main>
@@ -59,6 +70,7 @@ button {
   display: flex;
   text-align: center;
   justify-content: center;
+  position: bottom;
   border: 0px;
   color: black;
   background-color: coral;
@@ -67,32 +79,41 @@ button {
   margin: 25px;
   border-radius: 10px;
   font-size: 20px;
+  cursor: pointer;
 }
 .card {
-  width: 300px;
+  width: 200px;
   height: 300px;
   border: 3px;
   border-color: coral;
-  /* box-shadow: ; */
-  padding: 20px;
+  border-style: solid;
+  border-width: 0.5px;
+  padding: 10px;
   display: flex;
   flex-direction: column;
-  justify-content: space-between;
   margin-right: 20px;
   margin-bottom: 20px;
 }
 .card-container {
   display: flex;
   width: 100%;
-  grid-template-columns: 50px 1fr;
+  grid-template-columns: 10px 1fr;
+  margin-top: 20px;
 }
 .date {
   font-size: small;
   font-weight: bold;
-  padding-bottom: 20px;
+  padding-bottom: 10px;
   color: #4f9c7a;
+  text-align: center;
 }
+.del_button {
+  position: inherit;
+  display: flex;
+  justify-items: end;
+  justify-content: baseline;
 
+}
 /* header {
    line-height: 3.0; 
 } */
@@ -102,7 +123,7 @@ button {
   font-size: 25px;
   color: coral;
 }
-.memoTitle {
+.memo_title {
   color: coral;
   font-weight: bold;
   padding-bottom: 10px;
