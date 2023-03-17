6b61d94000e960b55fd7d48c6dc8eacde2268e28
robert <robert@unknown020fb576a7ca.attlocal.net>
2023-03-17 12:21:29 -0700

    added card and card styles

--- src/App.vue
+++ src/App.vue
@@ -1,30 +1,86 @@
 <script setup>
-import HelloWorld from './components/HelloWorld.vue'
-import TheWelcome from './components/TheWelcome.vue'
 </script>
 
 <template>
-  <header>
-    <img alt="Vue logo" class="logo" src="./assets/logo.svg" width="125" height="125" />
+  <h1>Keep track of your import memos</h1>
 
-    <div class="wrapper">
-      <HelloWorld msg="You did it!" />
-    </div>
+  
+  <header>
+    <h4 class='leadText'>Click to add memos</h4>
+    <button class="addMemos">+</button>
   </header>
 
-  <main>
-    <TheWelcome />
+  <main>  
+    <p class="your_memos">Memos:</p>
+      <div class="card-container">
+      <div class="card">
+        <p class="date">04/34/20002</p>
+        <p class="memoTitle">Title</p>
+        <p class="main-text">Lorem ipsum dolor sit amet consectetur, adipisicing elit. Ipsum culpa ab id dolorum inventore! Blanditiis culpa necessitatibus earum iure similique!</p>
+        <button>Delete</button>
+      </div>
+    </div>
   </main>
 </template>
 
 <style scoped>
 header {
-  line-height: 1.5;
+  /* line-height: 3.0; */
 }
 
-.logo {
-  display: block;
-  margin: 0 auto 2rem;
+
+
+button {
+  display: flex;
+  text-align: center;
+  justify-content: center;
+  border: 0px;
+  color: black;
+  background-color: coral;
+  width: 100px;
+  height: 30px;
+  margin: 25px;
+  border-radius: 10px;
+  font-size: 20px;
+}
+.card {
+  width: 300px;
+  height: 300px;
+  border: 3px;
+  border-color: coral;
+  /* box-shadow: ; */
+  padding: 20px;
+  display: flex;
+  flex-direction: column;
+  justify-content: space-between;
+  margin-right: 20px;
+  margin-bottom: 20px;
+}
+.card-container {
+  display: flex;
+  width: 100%;
+  grid-template-columns: 50px 1fr;
+}
+.date {
+  font-size: small;
+  font-weight: bold;
+  padding-bottom: 20px;
+  color: #4f9c7a;
+}
+.memoTitle {
+  color:coral;
+  font-weight: bold;
+  padding-bottom: 10px
+}
+.leadText {
+  font-weight: bold;
+  font-size: 25px;
+  color:coral;
+}
+.your_memos {
+  font-weight: bold;
+  font-size: 25px;
+
 }
 
 @media (min-width: 1024px) {
@@ -33,12 +89,7 @@ header {
     place-items: center;
     padding-right: calc(var(--section-gap) / 2);
   }
-
-  .logo {
-    margin: 0 2rem 0 0;
-  }
-
-  header .wrapper {
+    header .leadTezt {
     display: flex;
     place-items: flex-start;
     flex-wrap: wrap;
